71f42cf77b59cf8edfbd5643a6c468585b2f9da5
Irshit Mukherjee <irshitmukherjee@Irshits-MacBook-Air.local>
2025-03-12 11:43:54 +0530

    feat: navbar updated with titles

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -10,7 +10,7 @@ export default function Contact() {
         <ContactFormSection />
         <DirectContactSection />
       </div>
-      <div className="absolute bottom-[300px] left-[700px] bg-[radial-gradient(circle_at_center,#5D6EF3_0%,_transparent_40%)] opacity-30 w-full h-full"></div>
+      <div className="absolute bottom-[200px] left-[700px] bg-[radial-gradient(circle_at_center,#5D6EF3_0%,_transparent_40%)] opacity-30 w-full h-full"></div>
 
       <div className="absolute top-[100px] left-[250px] bg-[radial-gradient(circle_at_center,_#AA48FE_0%,_transparent_30%)] opacity-30 w-full h-full"></div>
     </div>
