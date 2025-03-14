5a6d0848f349505e4218891e7f432bd3e9a75763
MahenoorAhsan <mahenoor.ahsan@aot.edu.in>
2025-03-14 22:33:26 +0530

    resources design completed

---
+++ src/app/resources/page.tsx
@@ -0,0 +1,9 @@
+"use client"
+import Resources from "@/components/ui/resources/resources"
+export default function Contact(){
+    return (
+        <div className="scroll-smooth ">
+             <Resources/> 
+        </div>
+    )
+}
